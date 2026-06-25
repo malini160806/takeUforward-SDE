533a959b908894d366750066b6c1b922f2475035
malini160806 <maliniravanan@gmail.com>
2026-06-25 21:31:07 +0530

    Time: 1 ms (91.16%) | Memory: 57.6 MB (17.79%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.js
@@ -0,0 +1,21 @@
+var isValid = function(s) {
+    let stack = [];
+
+    const map = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
+    for (let ch of s) {
+        if (ch === '(' || ch === '[' || ch === '{') {
+            stack.push(ch);
+        } else {
+            if (stack.length === 0 || stack.pop() !== map[ch]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
