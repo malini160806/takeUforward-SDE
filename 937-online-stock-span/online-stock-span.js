b5506d71afa1797a4a116a0dac7a4af4a02b5fdd
malini160806 <maliniravanan@gmail.com>
2026-06-28 20:24:36 +0530

    Time: 58 ms (16.76%) | Memory: 72.6 MB (47.84%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.js
@@ -0,0 +1,17 @@
+class StockSpanner {
+    constructor() {
+        this.stack = []; 
+    }
+
+    next(price) {
+        let span = 1;
+
+        while (this.stack.length &&
+               this.stack[this.stack.length - 1][0] <= price) {
+            span += this.stack.pop()[1];
+        }
+
+        this.stack.push([price, span]);
+        return span;
+    }
+}
