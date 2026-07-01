08f01e6d451f0d9ff404074eaf5dd995f68fb359
malini160806 <maliniravanan@gmail.com>
2026-07-01 20:54:06 +0530

    Time: 2 ms (29.78%) | Memory: 54.7 MB (25.82%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,51 @@
+var strStr = function(haystack, needle) {
+    if (needle.length === 0) return 0;
+
+    const lps = computeLPS(needle);
+
+    let i = 0;
+    let j = 0;
+
+    while (i < haystack.length) {
+        if (haystack[i] === needle[j]) {
+            i++;
+            j++;
+
+            if (j === needle.length) {
+                return i - j;
+            }
+        } else {
+            if (j !== 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+
+    return -1;
+};
+
+function computeLPS(pattern) {
+    const lps = new Array(pattern.length).fill(0);
+
+    let len = 0;
+    let i = 1;
+
+    while (i < pattern.length) {
+        if (pattern[i] === pattern[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len !== 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+
+    return lps;
+}
