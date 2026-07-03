09aa9d878ceb1796eba6a7bc401b71b0b948a3b4
malini160806 <maliniravanan@gmail.com>
2026-07-03 21:21:42 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (29.67%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -0,0 +1,15 @@
+
+var inorderTraversal = function(root) {
+    const result = [];
+
+    function dfs(node) {
+        if (node === null) return;
+
+        dfs(node.left);
+        result.push(node.val);
+        dfs(node.right);
+    }
+
+    dfs(root);
+    return result;
+};
