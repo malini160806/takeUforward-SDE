933675f86275284072caea18a9d02b6442812cf8
malini160806 <maliniravanan@gmail.com>
2026-07-06 21:55:06 +0530

    Time: 8 ms (17.02%) | Memory: 58.4 MB (17.02%) - LeetSync

---
+++ 1222-remove-covered-intervals/remove-covered-intervals.js
@@ -0,0 +1,19 @@
+var removeCoveredIntervals = function(intervals) {
+
+    intervals.sort((a, b) => {
+        if (a[0] === b[0]) return b[1] - a[1];
+        return a[0] - b[0];
+    });
+
+    let count = 0;
+    let maxEnd = 0;
+
+    for (const [start, end] of intervals) {
+        if (end > maxEnd) {
+            count++;
+            maxEnd = end;
+        }
+    }
+
+    return count;
+};
