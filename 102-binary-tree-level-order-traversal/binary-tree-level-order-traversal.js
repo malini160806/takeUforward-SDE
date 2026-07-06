9b0b98b46a62d722d3d951084ae280e6d92be385
malini160806 <maliniravanan@gmail.com>
2026-07-06 21:52:04 +0530

    Time: 0 ms (100.00%) | Memory: 58.8 MB (51.85%) - LeetSync

---
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
@@ -0,0 +1,24 @@
+var levelOrder = function(root) {
+    if (!root) return [];
+
+    const result = [];
+    const queue = [root];
+
+    while (queue.length) {
+        const size = queue.length;
+        const level = [];
+
+        for (let i = 0; i < size; i++) {
+            const node = queue.shift();
+
+            level.push(node.val);
+
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+
+        result.push(level);
+    }
+
+    return result;
+};
