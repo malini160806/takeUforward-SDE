a09fad63414cb60b9802151858a6e1365f6d9405
malini160806 <maliniravanan@gmail.com>
2026-07-11 19:19:43 +0530

    Time: 69 ms (25.18%) | Memory: 65.2 MB (51.64%) - LeetSync

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,23 @@
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+
+    while (root) {
+
+        if (p.val < root.val && q.val < root.val) {
+            root = root.left;
+        }
+        else if (p.val > root.val && q.val > root.val) {
+            root = root.right;
+        }
+        else {
+            return root;
+        }
+    }
+
+    return null;
+};
